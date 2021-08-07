1123ee23773a856eac3a47ffe91688ca31f93b21
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-08-07 19:34:19 -0400

    Created Initial Frontend Files - Will be coding calculator app for Frontend Mentor with legacy React - Initial files are a combination of `npx create-react-app` and initial files for calculator-app - Also added a CSS reset https://meyerweb.com/eric/tools/css/reset/

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>H1 of Class React</h1>
+      </>
+    );
+  }
+}
+
+export default App;
